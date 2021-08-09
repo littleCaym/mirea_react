5cfa6f5f1dde3b304edf1656c9879cd764087772
Alexander <caymthecrow@gmail.com>
2021-08-09 19:11:48 +0300

    My First Try

---
+++ src/test/java/mirea/react/lab1_5/Lab15ApplicationTests.java
@@ -0,0 +1,13 @@
+package mirea.react.lab1_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
